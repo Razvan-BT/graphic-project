5aa2275999a1a4c74a3ed187a326bb81e4b10ca3
Razvan-BT <spatarurazvan8@gmail.com>
2022-09-02 17:13:14 +0300

    little changes

--- graphic.js
+++ graphic.js
@@ -37,149 +37,284 @@ function getDataNode() {
             success: function( data ){
                 // console.log('deceee');
                 for(let i = 0; i < Object.entries(data.checkpoints).length; i++) {
-                    getDataFromNodeRed(data.checkpoints[i], data.target)
+                    const TIME_WORK = 8,
+                    SET_FIRSTHOUR = 6,
+                    debug_status = false,
+                    MAX_INT_TIME = 30,
+                    OUT_DATE = 15;
+                let checkIfAreThere = 0,
+                    startMinut = 0,
+                    getTargetNumber,
+                    targetEveryNum = [],
+                    theHoursGet = 10,
+                    finishTime = 0,
+                    categoriesChart = new Array(), // legend - time in half hour: 6:00, 6:30, 7:30 ... 16:30 without brakes - 30 minutes.
+                    dataChart = [ ], // start with 0 minutes.
+                    remChart = new Array(); // black line
+            
+                getTargetNumber = Number(data.target);
+            
+                function hoursCalculated(chartTime, start) {
+                    let lucrate = (timestringtoDate(chartTime) - timestringtoDate(start)) / 1000;
+                    if (lucrate > 30600) lucrate = 28800; // time for 8.5 hours worked.
+                    else if (lucrate > 23400) lucrate = lucrate - 1800;
+                    else if (lucrate > 22800) lucrate = 21600;
+                    else if (lucrate > 12000) lucrate = lucrate - 1200;
+                    else if (lucrate > 10800) lucrate = 10800;
+                    return lucrate
+                }
+            
+                for (let g = 0; g < getTimesArray(6, OUT_DATE, 30).length; g++) {
+                    let chartTime = getTimesArray(6, OUT_DATE, 30)[g];
+                    const result = new Date(Math.floor(hmsToSecondsOnly(getTimesArray(6, OUT_DATE, 30)[g])) * 1000).toISOString().slice(11, 19);
+                    categoriesChart.push(result);
+            
+                    let d1 = new Date();
+                    let start = getTimesArray(6, OUT_DATE, 30)[0]
+                    let d2 = new Date();
+                    let now = new Date(d2.getFullYear(), d2.getMonth(), d2.getDate(), d2.getHours(), d2.getMinutes(), d2.getSeconds());
+                    let timenow = chartTime.substring(0, 8); // ultima actualizare la // scot data
+            
+                    timenow = timenow.substring(0, 5);  // scot secundele
+                    let getTime = timenow;
+                    if (debug_status) console.log("debug: getTimeWorked: " + getTime);
+                    if (debug_status) console.log("debug: now time: " + now);
+                    if (debug_status) console.log("debug: start time: " + start);
+            
+                    let lucrate = hoursCalculated(chartTime, start);
+                    const minlucrate = lucrate / 60;
+                    const target = getTargetNumber;
+            
+                    let targetnow = ((target / 480) * minlucrate);
+            
+                    if (now >= timestringtoDate(chartTime))
+                        dataChart.push(Math.round(targetnow));
+            
+                    if (finishTime >= 360) categoriesChart.pop();
+                    if (finishTime >= 360) dataChart.pop();
+                }
+            
+                /* Update de Y Axis (How blue line are calc from getTimeRemain)  */
+                for (let d = 0; d < dataChart.length; d++) { checkIfAreThere = d; }
+                if (theHoursGet > 0) theHoursGet += (checkIfAreThere * MAX_INT_TIME);
+                
+                // for (let j = 0; j <= dataChart.length - 1; j++) remChart.push(data.checkpoints[i]); // add black line from node.
+                
+                if (debug_status) console.log("debug: dataChart: " + dataChart.length);
+                if (debug_status) console.log("debug: checkIfAreThere: " + checkIfAreThere);
+            
+                var options = {
+                    series: [{
+                        name: "Target",
+                        data: dataChart,
+                    }, {
+                        name: "Rem",
+                        data: remChart,
+                    }],
+                    chart: {
+                        height: 350,
+                        type: 'line',
+                        dropShadow: {
+                            enabled: true,
+                            color: '#000',
+                            top: 18,
+                            left: 7,
+                            blur: 10,
+                            opacity: 0.2
+                        },
+                        toolbar: {
+                            show: false
+                        }
+                    },
+                    colors: ['#fa6934', '#545454'],
+                    dataLabels: {
+                        enabled: true,
+                    },
+                    stroke: {
+                        curve: 'smooth'
+                    },
+                    title: {
+                        text: `${Object.keys(data.checkpoints[i])}` + ` - last update (${checkUpdate()})`,
+                        align: 'center'
+                    },
+                    grid: {
+                        borderColor: '#e7e7e7',
+                        row: {
+                            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                            opacity: 0.5
+                        },
+                    },
+                    markers: {
+                        size: 1
+                    },
+                    xaxis: {
+                        categories: categoriesChart, // time
+                        title: {
+                            text: 'Time X-Axis (6:00 - 14:30)'
+                        }
+                    },
+                    yaxis: {
+                        title: {
+                            text: 'Time'
+                        },
+                        min: 0,
+                        max: theHoursGet  // set averange
+                    },
+                    legend: {
+                        position: 'top',
+                        horizontalAlign: 'right',
+                        floating: true,
+                        offsetY: -25,
+                        offsetX: -5
+                    }
+                };
+                    var dived = document.createElement('div');
+                    document.body.appendChild(dived);
+                    dived.id = "div_"+i;
+                    let chart = new ApexCharts(document.querySelector('#chart'), options);
+                    chart.render(); 
                 }   
             }
         });
     }
 }
-console.log(numarTotal)
-function getDataFromNodeRed(data, target) {
+// console.log(numarTotal)
+// function getDataFromNodeRed(data, target) {
 
-    const TIME_WORK = 8,
-        SET_FIRSTHOUR = 6,
-        debug_status = false,
-        MAX_INT_TIME = 30,
-        OUT_DATE = 15;
-    let checkIfAreThere = 0,
-        startMinut = 0,
-        getTargetNumber,
-        targetEveryNum = [],
-        theHoursGet = 10,
-        finishTime = 0,
-        categoriesChart = new Array(), // legend - time in half hour: 6:00, 6:30, 7:30 ... 16:30 without brakes - 30 minutes.
-        dataChart = [ ], // start with 0 minutes.
-        remChart = new Array(); // black line
+//     const TIME_WORK = 8,
+//         SET_FIRSTHOUR = 6,
+//         debug_status = false,
+//         MAX_INT_TIME = 30,
+//         OUT_DATE = 15;
+//     let checkIfAreThere = 0,
+//         startMinut = 0,
+//         getTargetNumber,
+//         targetEveryNum = [],
+//         theHoursGet = 10,
+//         finishTime = 0,
+//         categoriesChart = new Array(), // legend - time in half hour: 6:00, 6:30, 7:30 ... 16:30 without brakes - 30 minutes.
+//         dataChart = [ ], // start with 0 minutes.
+//         remChart = new Array(); // black line
 
-    getTargetNumber = Number(target);
+//     getTargetNumber = Number(target);
 
-    function hoursCalculated(chartTime, start) {
-        let lucrate = (timestringtoDate(chartTime) - timestringtoDate(start)) / 1000;
-        if (lucrate > 30600) lucrate = 28800; // time for 8.5 hours worked.
-        else if (lucrate > 23400) lucrate = lucrate - 1800;
-        else if (lucrate > 22800) lucrate = 21600;
-        else if (lucrate > 12000) lucrate = lucrate - 1200;
-        else if (lucrate > 10800) lucrate = 10800;
-        return lucrate
-    }
+//     function hoursCalculated(chartTime, start) {
+//         let lucrate = (timestringtoDate(chartTime) - timestringtoDate(start)) / 1000;
+//         if (lucrate > 30600) lucrate = 28800; // time for 8.5 hours worked.
+//         else if (lucrate > 23400) lucrate = lucrate - 1800;
+//         else if (lucrate > 22800) lucrate = 21600;
+//         else if (lucrate > 12000) lucrate = lucrate - 1200;
+//         else if (lucrate > 10800) lucrate = 10800;
+//         return lucrate
+//     }
 
-    for (let i = 0; i < getTimesArray(6, OUT_DATE, 30).length; i++) {
-        let chartTime = getTimesArray(6, OUT_DATE, 30)[i];
-        const result = new Date(Math.floor(hmsToSecondsOnly(getTimesArray(6, OUT_DATE, 30)[i])) * 1000).toISOString().slice(11, 19);
-        categoriesChart.push(result);
+//     for (let i = 0; i < getTimesArray(6, OUT_DATE, 30).length; i++) {
+//         let chartTime = getTimesArray(6, OUT_DATE, 30)[i];
+//         const result = new Date(Math.floor(hmsToSecondsOnly(getTimesArray(6, OUT_DATE, 30)[i])) * 1000).toISOString().slice(11, 19);
+//         categoriesChart.push(result);
 
-        let d1 = new Date();
-        let start = getTimesArray(6, OUT_DATE, 30)[0]
-        let d2 = new Date();
-        let now = new Date(d2.getFullYear(), d2.getMonth(), d2.getDate(), d2.getHours(), d2.getMinutes(), d2.getSeconds());
-        let timenow = chartTime.substring(0, 8); // ultima actualizare la // scot data
+//         let d1 = new Date();
+//         let start = getTimesArray(6, OUT_DATE, 30)[0]
+//         let d2 = new Date();
+//         let now = new Date(d2.getFullYear(), d2.getMonth(), d2.getDate(), d2.getHours(), d2.getMinutes(), d2.getSeconds());
+//         let timenow = chartTime.substring(0, 8); // ultima actualizare la // scot data
 
-        timenow = timenow.substring(0, 5);  // scot secundele
-        let getTime = timenow;
-        if (debug_status) console.log("debug: getTimeWorked: " + getTime);
-        if (debug_status) console.log("debug: now time: " + now);
-        if (debug_status) console.log("debug: start time: " + start);
+//         timenow = timenow.substring(0, 5);  // scot secundele
+//         let getTime = timenow;
+//         if (debug_status) console.log("debug: getTimeWorked: " + getTime);
+//         if (debug_status) console.log("debug: now time: " + now);
+//         if (debug_status) console.log("debug: start time: " + start);
 
-        let lucrate = hoursCalculated(chartTime, start);
-        const minlucrate = lucrate / 60;
-        const target = getTargetNumber;
+//         let lucrate = hoursCalculated(chartTime, start);
+//         const minlucrate = lucrate / 60;
+//         const target = getTargetNumber;
 
-        let targetnow = ((target / 480) * minlucrate);
+//         let targetnow = ((target / 480) * minlucrate);
 
-        if (now >= timestringtoDate(chartTime))
-            dataChart.push(Math.round(targetnow));
+//         if (now >= timestringtoDate(chartTime))
+//             dataChart.push(Math.round(targetnow));
 
-        if (finishTime >= 360) categoriesChart.pop();
-        if (finishTime >= 360) dataChart.pop();
-    }
+//         if (finishTime >= 360) categoriesChart.pop();
+//         if (finishTime >= 360) dataChart.pop();
+//     }
 
-    /* Update de Y Axis (How blue line are calc from getTimeRemain)  */
-    for (let i = 0; i < dataChart.length; i++) { checkIfAreThere = i; }
-    if (theHoursGet > 0) theHoursGet += (checkIfAreThere * MAX_INT_TIME);
+//     /* Update de Y Axis (How blue line are calc from getTimeRemain)  */
+//     for (let i = 0; i < dataChart.length; i++) { checkIfAreThere = i; }
+//     if (theHoursGet > 0) theHoursGet += (checkIfAreThere * MAX_INT_TIME);
     
-    //for (let j = 0; j <= dataChart.length - 1; j++) remChart.push(data.checkpoints[c]); // add black line from node.
+//     //for (let j = 0; j <= dataChart.length - 1; j++) remChart.push(data.checkpoints[c]); // add black line from node.
     
-    if (debug_status) console.log("debug: dataChart: " + dataChart.length);
-    if (debug_status) console.log("debug: checkIfAreThere: " + checkIfAreThere);
+//     if (debug_status) console.log("debug: dataChart: " + dataChart.length);
+//     if (debug_status) console.log("debug: checkIfAreThere: " + checkIfAreThere);
 
-    var options = {
-        series: [{
-            name: "Target",
-            data: dataChart,
-        }, {
-            name: "Rem",
-            data: remChart,
-        }],
-        chart: {
-            height: 350,
-            type: 'line',
-            dropShadow: {
-                enabled: true,
-                color: '#000',
-                top: 18,
-                left: 7,
-                blur: 10,
-                opacity: 0.2
-            },
-            toolbar: {
-                show: false
-            }
-        },
-        colors: ['#fa6934', '#545454'],
-        dataLabels: {
-            enabled: true,
-        },
-        stroke: {
-            curve: 'smooth'
-        },
-        title: {
-            text: `${Object.keys(data)}` + ` - last update (${checkUpdate()})`,
-            align: 'center'
-        },
-        grid: {
-            borderColor: '#e7e7e7',
-            row: {
-                colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-                opacity: 0.5
-            },
-        },
-        markers: {
-            size: 1
-        },
-        xaxis: {
-            categories: categoriesChart, // time
-            title: {
-                text: 'Time X-Axis (6:00 - 14:30)'
-            }
-        },
-        yaxis: {
-            title: {
-                text: 'Time'
-            },
-            min: 0,
-            max: theHoursGet  // set averange
-        },
-        legend: {
-            position: 'top',
-            horizontalAlign: 'right',
-            floating: true,
-            offsetY: -25,
-            offsetX: -5
-        }
-    };
-    let chart = new ApexCharts(document.querySelector("#chart"), options);
-    chart.render();  
-}
+//     var options = {
+//         series: [{
+//             name: "Target",
+//             data: dataChart,
+//         }, {
+//             name: "Rem",
+//             data: remChart,
+//         }],
+//         chart: {
+//             height: 350,
+//             type: 'line',
+//             dropShadow: {
+//                 enabled: true,
+//                 color: '#000',
+//                 top: 18,
+//                 left: 7,
+//                 blur: 10,
+//                 opacity: 0.2
+//             },
+//             toolbar: {
+//                 show: false
+//             }
+//         },
+//         colors: ['#fa6934', '#545454'],
+//         dataLabels: {
+//             enabled: true,
+//         },
+//         stroke: {
+//             curve: 'smooth'
+//         },
+//         title: {
+//             text: `${Object.keys(data)}` + ` - last update (${checkUpdate()})`,
+//             align: 'center'
+//         },
+//         grid: {
+//             borderColor: '#e7e7e7',
+//             row: {
+//                 colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+//                 opacity: 0.5
+//             },
+//         },
+//         markers: {
+//             size: 1
+//         },
+//         xaxis: {
+//             categories: categoriesChart, // time
+//             title: {
+//                 text: 'Time X-Axis (6:00 - 14:30)'
+//             }
+//         },
+//         yaxis: {
+//             title: {
+//                 text: 'Time'
+//             },
+//             min: 0,
+//             max: theHoursGet  // set averange
+//         },
+//         legend: {
+//             position: 'top',
+//             horizontalAlign: 'right',
+//             floating: true,
+//             offsetY: -25,
+//             offsetX: -5
+//         }
+//     };
+//     let chart = new ApexCharts(document.querySelector("#chart"), options);
+//     chart.render();  
+// }
 
 function timestringtoDate(timestring) {
     let d = new Date(); // Creates a Date Object using the clients current time
