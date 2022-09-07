b6d1b045159279795311e9a0878f0c0ff3bf07a3
Razvan-BT <spatarurazvan8@gmail.com>
2022-09-07 08:57:18 +0300

    new graph

--- src/js/apexgraph.js
+++ src/js/apexgraph.js
@@ -122,6 +122,7 @@ $(document).ready(function () {
                                     }
                                     while(dataChart.length < realChart[i].length);
                                 }
+                               
 
                                 if (debug_status) console.log("debug: dataChart: " + dataChart.length);
                                 if (debug_status) console.log("debug: checkIfAreThere: " + checkIfAreThere);
@@ -170,86 +171,53 @@ $(document).ready(function () {
                                 //     }
                                 //   }]
                                 // };
-                                var options = {
-                                    series: [{
-                                    name: 'Inflation',
-                                    data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
-                                  }],
-                                    chart: {
-                                    height: 350,
-                                    type: 'bar',
-                                  },
-                                  plotOptions: {
-                                    bar: {
-                                      borderRadius: 10,
-                                      dataLabels: {
-                                        position: 'top', // top, center, bottom
-                                      },
-                                    }
-                                  },
-                                  dataLabels: {
-                                    enabled: true,
-                                    formatter: function (val) {
-                                      return val + "%";
-                                    },
-                                    offsetY: -20,
-                                    style: {
-                                      fontSize: '12px',
-                                      colors: ["#304758"]
-                                    }
-                                  },
-                                  
-                                  xaxis: {
-                                    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                                    position: 'top',
-                                    axisBorder: {
-                                      show: false
-                                    },
-                                    axisTicks: {
-                                      show: false
-                                    },
-                                    crosshairs: {
-                                      fill: {
-                                        type: 'gradient',
-                                        gradient: {
-                                          colorFrom: '#D8E3F0',
-                                          colorTo: '#BED1E6',
-                                          stops: [0, 100],
-                                          opacityFrom: 0.4,
-                                          opacityTo: 0.5,
-                                        }
-                                      }
-                                    },
-                                    tooltip: {
-                                      enabled: true,
-                                    }
-                                  },
-                                  yaxis: {
-                                    axisBorder: {
-                                      show: false
-                                    },
-                                    axisTicks: {
-                                      show: false,
-                                    },
-                                    labels: {
-                                      show: false,
-                                      formatter: function (val) {
-                                        return val + "%";
-                                      }
-                                    }
-                                  
-                                  },
-                                  title: {
-                                    text: 'Monthly Inflation in Argentina, 2002',
-                                    floating: true,
-                                    offsetY: 330,
-                                    align: 'center',
-                                    style: {
-                                      color: '#444'
-                                    }
-                                  }
-                                };
+                                // am facuto si pe asta. :D
 
+                                let dataCheck = [];
+                                for(let y = 0; y < dataChart.length; y++) {
+                                  dataCheck[y] = {
+                                    x: categoriesChart[y],
+                                    y: realChart[i][y],
+                                    goals: [
+                                      {
+                                        name: 'Target count',
+                                        value: dataChart[y],
+                                        strokeHeight: 5,
+                                        strokeColor: '#775DD0'
+                                      }
+                                    ]
+                                  };
+                                }
+
+                                var options = {
+                                  series: [
+                                  {
+                                    name: 'Target count',
+                                    data: dataCheck
+                                  }
+                                ],
+                                  chart: {
+                                  height: 350,
+                                  type: 'bar'
+                                },
+                                plotOptions: {
+                                  bar: {
+                                    columnWidth: '10%'
+                                  }
+                                },
+                                colors: ['#00E396'],
+                                dataLabels: {
+                                  enabled: false
+                                },
+                                legend: {
+                                  show: true,
+                                  showForSingleSeries: true,
+                                  customLegendItems: ['Time count', 'Real count'],
+                                  markers: {
+                                    fillColors: ['#00E396', '#775DD0']
+                                  }
+                                }
+                              };
                                 /* create element forEach table */
                                 let diver = document.createElement('div');
                                 diver.id = 'num_'+i;
